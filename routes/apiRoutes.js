006b2d7d2e6a8fd8e4fe7a39dc8d08b1920c5a26
Nikita <jrringler13759@gmail.com>
2020-05-23 19:06:44 -0400

    couldn't dry the rest

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,16 @@
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 
-const readFile = (res) => {
-  fs.readFile("db/db.json", "utf8", function(err, data){
-    if(err)throw err;
-    return res.json(JSON.parse(data));
-  })
-}
 
 module.exports = function(app) {
   
   // API GET Requests
   app.get("/api/notes", function(req, res) {
 
-    readFile(res);
+    fs.readFile("db/db.json", "utf8", function(err, data){
+      if(err)throw err;
+      res.json(JSON.parse(data));
+    })
   });
 
 
